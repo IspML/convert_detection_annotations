93f9e6b91d6a273c7a50bfb1073e6e183bcc763e
Benjamin Wang <Benjamin.mj.wang@gmail.com>
2020-06-19 10:47:57 +0800

    Add files via upload

---
+++ mycsv2labelme_new.py
@@ -0,0 +1,73 @@
+'''
+This script is used for converting SKU110K dataset annotations (in csv)
+to the LabelMe format (in json).
+
+By BW
+
+forked from https://github.com/spytensor/prepare_detection_dataset
+'''
+
+import os
+import cv2
+import json
+import base64
+import numpy as np
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+from IPython import embed
+from labelme import utils
+
+
+def converting(csv_file, image_path):
+    ''' only 1 function is defined and used here, for converting all the things.  parameters? sorry. '''
+    #
+    annotations = pd.read_csv(csv_file, header=None) # filename,xmin,ymin,xmax,ymax,label,width,height for SKU110K
+    #
+    total_csv_annotations = {}
+    total_csv_size = {}
+    for annotation in annotations.values:
+        key = annotation[0].split(os.sep)[-1] # to obtain the real filename, ie, to delete the parental folders.
+        value = annotation[1:6]
+        width, height = annotation[6:]
+        if key in total_csv_annotations.keys():
+            total_csv_annotations[key].append(value)
+        else:
+            total_csv_annotations[key] = [value]
+            total_csv_size[key] = (width, height)
+    # -.-
+    # -.-
+    for key, value in total_csv_annotations.items():
+        width, height = total_csv_size[key]
+        #
+        path2key = image_path+key
+        if not os.path.exists(path2key): continue
+        with open(path2key, "rb") as f:
+            labelme_format = {
+                "version": "3.6.16",
+                "flags": {},
+                "lineColor": [0, 255, 0, 128],
+                "fillColor": [255, 0, 0, 128],
+                "imagePath": key,
+                "imageHeight": height,
+                "imageWidth": width,
+                "imageData": base64.b64encode(f.read()).decode('utf-8'),
+                "shapes": [{
+                        "label": label[4],
+                        "line_color": None,
+                        "fill_color": None,
+                        "shape_type": "rectangle",
+                        "points": [[label[0], label[1]], [max(label[2], width-1), max(label[3], height-1)]]
+                    } for label in value]
+            }
+            #
+            json.dump(labelme_format, open("%s%s"%(image_path, key.replace(".jpg",".json")), "w"), ensure_ascii=False, indent=2)
+
+
+root_path = '/root/bw/datasets/SKU110K_fixed/'
+image_path = root_path + "images/"
+for csv_file in [root_path + "annotations/annotations_test.csv",
+                 root_path + "annotations/annotations_train.csv",
+                 root_path + "annotations/annotations_val.csv"]:
+    print('\n\nProcessing', csv_file)
+    converting(csv_file, image_path)
