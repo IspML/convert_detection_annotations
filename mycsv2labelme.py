c96da6f4c31f9a2d2635ffe336090feb9fb75f77
Benjamin Wang <Benjamin.mj.wang@gmail.com>
2020-06-23 15:04:34 +0800

    Update and rename mycsv2labelme_new.py to mycsv2labelme.py

--- mycsv2labelme.py
+++ mycsv2labelme.py
@@ -4,7 +4,7 @@ to the LabelMe format (in json).
 
 By BW
 
-forked from https://github.com/spytensor/prepare_detection_dataset
+Do process the jpgs with `check_jpg()` first!
 '''
 
 import os
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from tqdm import tqdm
+from PIL import Image
 from IPython import embed
 from labelme import utils
 
@@ -42,26 +43,24 @@ def converting(csv_file, image_path):
         #
         path2key = image_path+key
         if not os.path.exists(path2key): continue
-        with open(path2key, "rb") as f:
-            labelme_format = {
-                "version": "3.6.16",
-                "flags": {},
-                "lineColor": [0, 255, 0, 128],
-                "fillColor": [255, 0, 0, 128],
-                "imagePath": key,
-                "imageHeight": height,
-                "imageWidth": width,
-                "imageData": base64.b64encode(f.read()).decode('utf-8'),
-                "shapes": [{
-                        "label": label[4],
-                        "line_color": None,
-                        "fill_color": None,
-                        "shape_type": "rectangle",
-                        "points": [[label[0], label[1]], [max(label[2], width-1), max(label[3], height-1)]]
-                    } for label in value]
-            }
-            #
-            json.dump(labelme_format, open("%s%s"%(image_path, key.replace(".jpg",".json")), "w"), ensure_ascii=False, indent=2)
+        labelme_format = {
+            "version": "3.6.16",
+            "flags": {},
+            "lineColor": [0, 255, 0, 128],
+            "fillColor": [255, 0, 0, 128],
+            "imagePath": key,
+            "imageHeight": height,
+            "imageWidth": width,
+            "imageData": base64.b64encode(np.asarray(Image.open(path2key)).decode('utf-8'),
+            "shapes": [{
+                    "label": label[4],
+                    "line_color": None,
+                    "fill_color": None,
+                    "shape_type": "rectangle",
+                    "points": [[label[0], label[1]], [max(label[2], width-1), max(label[3], height-1)]]
+                } for label in value]
+        }
+        json.dump(labelme_format, open("%s%s"%(image_path, key.replace(".jpg",".json")), "w"), ensure_ascii=False, indent=2)
 
 
 root_path = '/root/bw/datasets/SKU110K_fixed/'
